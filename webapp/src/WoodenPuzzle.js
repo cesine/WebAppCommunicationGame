bd40649c812c1ca7e38109fa9fb3581ff9ef7e0e
cesine <cesine@host>
2011-11-23 17:15:35 -0500

    tutorial for setting up testing with textmate

--- webapp/src/WoodenPuzzle.js
+++ webapp/src/WoodenPuzzle.js
@@ -1,3 +1,21 @@
+/*global MT, jQuery
+*/
+var MT = MT || {};
+
+(function () {
+	MT.ajax_service = {
+		contact_server: function (url, params){
+			jQuery.getJSON(url, params);
+		},
+		save_preferences:function (key, value) {
+			this.contact_server("/save_preferences.php",{key: key, value: value});
+		}
+	};
+	
+}(jQuery));
+
+
+
 myapp = {};
 
 myapp.Greeter = function() { };
@@ -5,3 +23,4 @@ myapp.Greeter = function() { };
 myapp.Greeter.prototype.greet = function(name) {
   return "Hello " + name + "!";
 };
+
