1aae6af14f6fda1876cfb75422d2405dae09310e
cesine <cesine@host>
2011-11-24 13:24:42 -0500

    there is no way they mean webworkers

--- test/WoodenPuzzleTest.js
+++ test/WoodenPuzzleTest.js
@@ -8,11 +8,32 @@ GreeterTest.prototype.testGreet = function() {
 
 GreeterTest.prototype.testExample = function () {
   expectAsserts(1);
-  var worker = new Worker();
+  var worker = new Worker('worker.js');
+
+  //from http://code.google.com/p/js-test-driver/wiki/TestCase
   var doSomething = {};
   worker.listener = function (work){
     assertSame(doSomething, work);
   };
-  worker.perform(doSomething);
+  //worker.perform(doSomething);
+
+
+
+  //from http://www.html5rocks.com/en/tutorials/workers/basics/
+  worker.addEventListener('message', function(e) {
+    document.getElementById('result').textContent = e.data;
+  }, false);
+
+  worker.postMessage({'cmd': 'start', 'msg': 'Hi'});
+
+  
+  /*
+  Conclusion: The code in http://code.google.com/p/js-test-driver/wiki/TestCase for Worker
+  isnt talking about WebWorkers... but rather a fake class.
+   */
+
+
+
+
 };
 
