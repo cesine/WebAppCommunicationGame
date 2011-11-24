ed0ed18c2a24184a6ace5d72f8f0b739f1c6ad2a
cesine <cesine@host>
2011-11-24 16:15:29 -0500

    copying test to src-test

--- src-test/WoodenPuzzleTest.js
+++ src-test/WoodenPuzzleTest.js
@@ -1,73 +1,40 @@
-/*
-TestCase('TestPuzzlePiece', sinon.testCase({
 
-  "test should be an object": function(){
-    assertObject(WOODENSHAPEGAME.puzzlePiece);
-  }
+AsyncTestCase('TestPiecePositionHistory', {
+  testSequenceOfActions: function(queue){
+    var state = 0;
 
-}));*/
+    queue.call('Step 1: create a puzzle piece', function() {
+      assertEquals(0, state);
+    });
 
-GreeterTest = TestCase("GreeterTest");
- 
-GreeterTest.prototype.testGreet = function() {
-  var greeter = new myapp.Greeter();
-  assertEquals("Hello World!", greeter.greet("World"));
-};
-
-
-
-
-var QueueTest = AsyncTestCase('QueueTest');
-
-/*
-The AsyncTestCase extends TestCase by adding a
-parameter that is a queue that accepts inline functions
-that represent sequential steps of the test. 
-*/
-QueueTest.prototype.testSomething = function(queue) {
-  var state = 0;
-
-  queue.call('Step 1: assert the starting condition holds', function() {
-    assertEquals(0, state);
-  });
-
-  queue.call('Step 2: increment our variable', function() {
-    ++state;
-  });
-
-  queue.call('Step 3: assert the variable\'s value changed', function() {
-    assertEquals(1, state);
-  });
-};
-
-/*As the test runner executes each step in the queue
-it passes a parameter to that step.
-The parameter is an empty pool of callback functions. 
-* */
-var AsynchronousTest = AsyncTestCase('AsynchronousTest');
-
-AsynchronousTest.prototype.testSomethingComplicated = function(queue) {
-  var state = 0;
-
-  queue.call('Step 1: schedule the window to increment our variable 5 seconds from now.', function(callbacks) {
-//You call var myTweakedCallback = callbacks.add(myOriginalCallback)
-    var myCallback = callbacks.add(function() {
+    queue.call('Step 2: set its position to 100,100', function() {
       ++state;
     });
 
-    window.setTimeout(myCallback, 5000);
-  });
+    queue.call('Step 3: assert its position value changed', function() {
+      assertEquals(1, state);
+    });
+  },
+  testSomethingComplicated: function(queue){
+    var state2 = 0;
 
+    queue.call('Step 1: schedule the window to increment our variable 5 seconds from now.', function(callbacks) {
+      //You call var myTweakedCallback = callbacks.add(myOriginalCallback)
+      var myCallback = callbacks.add(function() {
+        ++state2;
+      });
 
-  /* Here is where the above callback gets tested
-  * will not execute any subsequent step in the queue
-  * until all outstanding callbacks of the current step are complete.
-  * f the callbacks are not a called for an egregious amount of time,
-  * currently set to 30 seconds
-  * */
-  queue.call('Step 2: then assert our state variable changed', function() {
-    assertEquals(1, state);
-  });
-};
-
+      window.setTimeout(myCallback, 5000);
+    });
+    /* Here is where the above callback gets tested
+     * will not execute any subsequent step in the queue
+     * until all outstanding callbacks of the current step are complete.
+     * f the callbacks are not a called for an egregious amount of time,
+     * currently set to 30 seconds
+     * */
+    queue.call('Step 2: then assert our state variable changed', function() {
+      assertEquals(1, state2);
+    });
+  }
+});
 
