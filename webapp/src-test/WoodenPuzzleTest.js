e7cb5765668aea14018b6ec4ae4fbb6154a33fe0
cesine <cesine@host>
2011-11-24 08:40:51 -0500

    exploring using eclipse
    originally tried to use TextMate to follow the macros from zombietdd, but switched back to vim.
    
    following demo from http://www.youtube.com/watch?v=zsCYkmAJBAY
    
     giving eclipse a try (although i still feel vim is lighter)
    *aptana studio for js library autocomplete etc
    ** http://www.aptana.com/downloads/start
    
    *jstestdriver plugin for eclipse (1 year old?)
    ** http://code.google.com/p/js-test-driver/wiki/UsingTheEclipsePlugin

--- webapp/src-test/WoodenPuzzleTest.js
+++ webapp/src-test/WoodenPuzzleTest.js
@@ -5,16 +5,3 @@ GreeterTest.prototype.testGreet = function() {
   assertEquals("Hello World!", greeter.greet("World"));
 };
 
-TestCase("ExampleTest", sinon.testCase({
-	"test should do stuff": function (stub, mock){
-		//stub(jQuery, "getJSON");
-		MT.ajax_service.contact_server("url", {param: 123});
-		//assert(jQuery.getJSON.called);
-	},
-	"test should save preferences": function(stub, mock) {
-		stub(MT.ajax_service, "contact_server");
-		MT.ajax_service.save_preferences("help","off");
-		assert(MT.ajax_service.contact_server.calledWith("/save_preferences.php", {key: "help", value: "off"}));
-	}
-	
-}));
--- webapp/src/WoodenPuzzle.js
+++ webapp/src/WoodenPuzzle.js
@@ -1,21 +1,3 @@
-/*global MT, jQuery
-*/
-var MT = MT || {};
-
-(function () {
-	MT.ajax_service = {
-		contact_server: function (url, params){
-			jQuery.getJSON(url, params);
-		},
-		save_preferences:function (key, value) {
-			this.contact_server("/save_preferences.php",{key: key, value: value});
-		}
-	};
-	
-}(jQuery));
-
-
-
 myapp = {};
 
 myapp.Greeter = function() { };
