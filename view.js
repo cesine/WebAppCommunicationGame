b5bc41548d04121203b546436b9faaa4883ab988
cesine <cesine@host>
2011-11-25 21:45:37 -0500

    added jquery
    to deal with drawing the image on the canvas after it has loaded

--- view.js
+++ view.js
@@ -1,15 +1,20 @@
 initCanvas = function() {
   canvasWidth = 600;
   canvasHeight = 400;
-  viewstimulus = document.getElementById("viewStimulus");
-  viewstimulus.setAttribute("width", canvasWidth);
-  viewstimulus.setAttribute("height", canvasHeight);
-//viewstimulus.setAtt.ibute('style', 'background-color: #999;');
-  //viewstimulus.style.position = "relative";
-  //viewstimulus.style.zindex = 1;
-  window.ctx = viewstimulus.getContext("2d");
+  gamearea = document.getElementById("gamearea_canvas");
+  gamearea.setAttribute("width", canvasWidth);
+  gamearea.setAttribute("height", canvasHeight);
+  gamearea.setAttribute('style', 'background-color: #999;');
+  //gamearea.style.position = "relative";
+  //gamearea.style.zindex = 1;
+  window.ctx = gamearea.getContext("2d");
 
   ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-//loadButton();
+  var backgroundimage = new Image();
+  backgroundimage.src = "images/wood_golden.jpg";
+  $(backgroundimage).load(function() {
+    ctx.drawImage(backgroundimage, 0, 0, canvasWidth, canvasHeight);
+  });
+
   return ctx;
 }
