9393dbdbd56bdf43b40842b07f33ebe52e8519e6
cesine <cesine@host>
2011-11-26 08:37:45 -0500

    added android architecture

---
+++ src/ca/ilanguage/communicationgame/AndroidCommunicationGameActivity.java
@@ -0,0 +1,13 @@
+package ca.ilanguage.communicationgame;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AndroidCommunicationGameActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
