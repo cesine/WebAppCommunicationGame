994f08961a0848f87ad269e6442b132dfbc21e8d
cesine <cesine@host>
2011-11-30 15:09:58 -0500

    loading game set from external file

--- assets/webapp/main.js
+++ assets/webapp/main.js
@@ -1,7 +1,43 @@
 
+var playWoodenPuzzle = function() {
+  var game = new WoodenPuzzleSet();
+
+  game.canvas = document.getElementById('surface');
+  game.canvas.setAttribute("width", window.innerWidth);
+  game.canvas.setAttribute("height", window.innerHeight);
+  game.ctx = game.canvas.getContext('2d');
+
+
+  game.ASSET_MANAGER = new AssetManager();
+  var pieces = gameset.pieces;
+  for (var j = 0; j < pieces.length; j++){
+    game.ASSET_MANAGER.queueDownload(pieces[j]);
+  }
+  /*
+   Allow programmer to turn sound off (so game will still play even if Flash security permissions havent been set
+   see console_log_output.txt for more info
+   TODO turn into a UI element (button) for user to understand what is going on
+   */
+  game.soundSwitch= "off";
+  console.log("Sound is switched " + game.soundSwitch);
+  if(game.soundSwitch !== "off"){
+    game.ASSET_MANAGER.queueSound('alien-boom', 'audio/alien_boom.mp3');
+    game.ASSET_MANAGER.queueSound('bullet-boom', 'audio/bullet_boom.mp3');
+    game.ASSET_MANAGER.queueSound('bullet', 'audio/bullet.mp3');
+  }
+
+  game.ASSET_MANAGER.downloadAll(function() {
+    game.init(game.ctx);
+    game.start();
+  });
+
+  return game;
+
+
+};
+
+
 var playAliens = function() {
-
-
   var game = new WoodenPuzzleSet();
 
   game.canvas = document.getElementById('surface');
@@ -42,6 +78,5 @@ var playAliens = function() {
 };
 
 
-
-var game6 = playAliens();
-game6.pauseGame();
+var game4 = playWoodenPuzzle();
+//game4.pauseGame();
