e1f40f1cfeb6ef84af937fc84b83c6b40cf0747e
cesine <cesine@host>
2011-11-30 11:15:15 -0500

    refactored AssetManager to seperate file

--- assets/webapp/view.js
+++ assets/webapp/view.js
@@ -4,16 +4,7 @@ gp.game1;
 gp.game2;
 gp.game3;
 gp.game4;
-gp.ASSET_MANAGER = new AssetManager();
 
-gp.ASSET_MANAGER.queueDownload('images/wood_golden.jpg');
-gp.ASSET_MANAGER.queueDownload('images/canard.png');
-
-gp.ASSET_MANAGER.downloadAll(function() {
-  var sprite = gp.ASSET_MANAGER.getAsset('images/wood_golden.jpg');
-  //gamearea.translate(canvas.width/2, canvas.height/2);
-  gamearea.drawImage(sprite, 0, 0, sprite.width, sprite.height)
-});
 
 initCanvas = function() {
 
